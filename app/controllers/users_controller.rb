163639fc66300594b400699062bd6c11776ed93c
Christiana Vandermale <christianavdm@gmail.com>
2019-06-11 02:40:36 +0000

    Edited login routes on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   get '/login' do
     if session[:user_id]
-      redirect :'/tweets'
+      redirect :'/homepage'
     else
       erb :'/users/login'
     end
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/tweets'
+      redirect '/homepage'
     else
       redirect '/login'
     end
